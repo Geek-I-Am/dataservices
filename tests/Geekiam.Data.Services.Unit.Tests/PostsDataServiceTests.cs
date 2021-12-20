a38d7df4884261963de32673b3a63d0655aa3e23
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-12-20 19:44:28 +0000

    unit tests added

---
+++ tests/Geekiam.Data.Services.Unit.Tests/PostsDataServiceTests.cs
@@ -0,0 +1,43 @@
+using AutoMapper;
+using Moq;
+using Shouldly;
+using Threenine.Data;
+using Xunit;
+
+namespace Geekiam.Data.Services.Unit.Tests;
+
+public class PostsDataServiceTests
+{
+    private PostsDataService _classUnderTest;
+    private Mock<IMapper> _mapper = new();
+    private Mock<IUnitOfWork> _unitOfWork = new();
+
+    public PostsDataServiceTests()
+    {
+        _classUnderTest = new PostsDataService(_mapper.Object, _unitOfWork.Object);
+    }
+    
+    [Theory]
+    [InlineData("poo poo", "PooPoo")]
+    [InlineData("software development", "SoftwareDevelopment")]
+    [InlineData("geek i am", "GeekIAm")]
+    [InlineData("three  little  birds", "ThreeLittleBirds")]
+    public void ShouldTransformTags(string testString, string expected)
+    {
+        var result = _classUnderTest.TransformTag(testString);
+        
+        result.ShouldBeEquivalentTo(expected);
+    }
+    
+    [Theory]
+    [InlineData("poo poo", "poo-poo")]
+    [InlineData("software development", "software-development")]
+    [InlineData("geek i am", "geek-i-am")]
+    [InlineData("three  little  birds", "three-little-birds")]
+    public void ShouldTransformPermalink(string testString, string expected)
+    {
+        var result = _classUnderTest.TransformPermalink(testString);
+        
+        result.ShouldBeEquivalentTo(expected);
+    }
+}
