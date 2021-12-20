3ce65aa0f6e039bb44d261212860037fdb2fe2e1
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-12-20 18:46:42 +0000

    added formatting functions

--- src/Geekiam.Data.Services/PostsDataService.cs
+++ src/Geekiam.Data.Services/PostsDataService.cs
@@ -1,9 +1,12 @@
-﻿using AutoMapper;
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+using AutoMapper;
 using Geekiam.Database.Entities;
 using Geekiam.Domain.Requests.Posts;
 using Geekiam.Domain.Responses.Posts;
 using Threenine.Data;
-
+[assembly: InternalsVisibleTo("Geekiam.Data.Services.Unit.Tests")]
 namespace Geekiam.Data.Services;
 
 public class PostsDataService : IDataService<Submission, Submitted>
@@ -24,4 +27,56 @@ public class PostsDataService : IDataService<Submission, Submitted>
         await _unitOfWork.CommitAsync();
         return new Submitted() { Article = new Article(article.Title, new Uri(article.Url)) };
     }
+
+    internal void SaveTags(List<string> tags)
+    {
+        tags.ForEach(tag =>
+        {
+            var newtag = TransformTag(tag);
+            var permalink = TransformPermalink(tag);
+            
+            
+
+        });
+    }
+
+    internal string TransformTag(string tag)
+    {
+        var sb = new StringBuilder();
+
+        var words = tag.Trim().Split(' ');
+
+        if (words.Length < 1) return sb.Append(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(tag.ToLower().Trim())).ToString();
+        foreach (var word in words)
+        {
+            sb.Append(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word.Trim().ToLower()));
+        }
+
+        return sb.ToString();
+    }
+    
+    internal string TransformPermalink(string tag)
+    {
+        var sb = new StringBuilder();
+
+        var words = tag.Trim().Split(' ');
+
+        if (words.Length < 1) return sb.Append(tag.ToLower().Trim()).ToString();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (i == words.Length)
+            {
+                sb.Append($"{words[i]}");
+            }
+            else
+            {
+                if(!string.IsNullOrWhiteSpace(words[i])) sb.Append($"{words[i]}-");
+            }
+        }
+
+        return sb.ToString().TrimEnd('-');
+    }
+    
+    
 }
--- src/Geekiam.Domain.Requests/Posts/Submission.cs
+++ src/Geekiam.Domain.Requests/Posts/Submission.cs
@@ -5,7 +5,7 @@ namespace Geekiam.Domain.Requests.Posts;
 public class Submission
 {
     public Detail Article { get; set; }
-    public IList<Section> Tags { get; set; }
-    public IList<Section> Categories { get; set; }
+    public IList<string> Tags { get; set; }
+    public IList<string> Categories { get; set; }
     
 }
